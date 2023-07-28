5c04a96e027965d09f346be3dd5ba4b4423b6e72
Manavik24 <manaviik.24@gmail.com>
2023-07-28 16:09:03 +0530

    Button Color Changed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-
-import React from 'react';
+import React from "react";
 
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-100 flex justify-center items-center">
       <div className="bg-white p-8 rounded-lg shadow-md">
-        <h1 className="text-4xl font-bold mb-4">Welcome to coding circle website!!</h1>
-        <p className="text-gray-700">This is an example of using Tailwind CSS in a React App.</p>
+        <h1 className="text-4xl font-bold mb-4">
+          Welcome to coding circle website!!
+        </h1>
+        <p className="text-gray-700">
+          This is an example of using Tailwind CSS in a React App.
+        </p>
         <div className="mt-6">
-          <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg">
+          <button className="bg-green-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg">
             click me
           </button>
         </div>
