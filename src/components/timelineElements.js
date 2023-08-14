32c0e9086c4d302f37c4ffbf285f8cb27eee4b4e
vishwajeet shinde <shindevishwajeet56@gmail.com>
2023-08-14 15:05:18 +0530

    Roadmap

---
+++ src/components/timelineElements.js
@@ -0,0 +1,35 @@
+let timelineElements = [
+  {
+    id: 1,
+    title: "Step 1 ",
+    description: " Learn HTML, CSS, and JavaScript",
+  },
+  {
+    id: 2,
+    title: "Step 2",
+    description: "Study Data Structures and Algorithms",
+  },
+  {
+    id: 3,
+    title: "Step 3",
+    description: " Dive into Front-end Development (React, Angular, etc.)",
+  },
+  {
+    id: 4,
+    title: "Step 4",
+    description: "Explore Back-end Technologies (Node.js, Express, etc.) ",
+  },
+  {
+    id: 5,
+    title: "Step 5",
+    description: " Learn Database Management (SQL, NoSQL, etc.)",
+  },
+  {
+    id: 6,
+    title: "Step 6",
+    description: "Explore in Competitive programming and Data Structures",
+  },
+];
+
+export default timelineElements;
+
