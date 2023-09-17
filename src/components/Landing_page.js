e28fd4f03e2afb67dd73f9aef3cf737e4b1527d9
mansi0829 <mansigundre1@gmail.com>
2023-09-17 23:42:23 +0530

    langing page changes

--- src/components/Landing_page.js
+++ src/components/Landing_page.js
@@ -1,83 +1,295 @@
-import React, { useEffect, useState } from 'react';
-import '../index';
+import React from "react";
 
-
-const HeroSection = () => {
+const Landing_page = () => {
   return (
-    <header className="bg-gray-800 text-white text-center py-16">
-      <div className="container mx-auto">
-        <h1 className="text-4xl font-bold mb-4">Welcome to Coding Circle</h1>
-        <p className="text-lg">Learn, Code, and Connect with fellow coders and seniors!</p>
-        <div className="mt-8">
-          <a href="#" className="bg-white text-blue-600 font-bold py-2 px-6 rounded-full hover:bg-blue-200">
-            Get Started
-          </a>
+    <div class="bg-gray-800 text-white">
+      <section class="px-2 py-32  md:px-0">
+        <div class="container items-center max-w-6xl px-8 mx-auto xl:px-5">
+          <div class="flex flex-wrap items-center sm:-mx-3">
+            <div class="w-full md:w-1/2 md:px-3">
+              <div class="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
+                <h1 class="text-4xl font-extrabold tracking-tight text-white-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
+                  <span class="block xl:inline">
+                    Here is a guide for you to direct you on the roadmap of{" "}
+                  </span>
+                  <span class="block text-indigo-600 xl:inline">
+                    Success....
+                  </span>
+                </h1>
+                <p class="mx-auto text-base text-white-500 sm:max-w-md lg:text-xl md:max-w-3xl">
+                  Learn, Code, and Connect with fellow coders and seniors!
+                </p>
+                <div class="relative flex flex-col sm:flex-row sm:space-x-4">
+                  <a
+                    href="#_"
+                    class="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-indigo-600 rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto"
+                  >
+                    Join Now
+                  </a>
+                  <a
+                    href="#_"
+                    class="flex items-center px-6 py-3 text-gray-500 bg-gray-100 rounded-md hover:bg-gray-200 hover:text-white-600"
+                  >
+                    Learn More about PCSB
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="w-full md:w-1/2">
+              <div class="w-full h-auto overflow-hidden rounded-md shadow-xl sm:rounded-xl">
+                <img src="https://images.unsplash.com/photo-1498049860654-af1a5c566876?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" />
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </header>
-  );
-};
+      </section>
 
-const FeaturesSection = () => {
-  return (
-    <section className="py-16 px-10">
-      <div className="container mx-auto">
-        <h2 className="text-3xl font-bold mb-8 text-white text-center">Why Choose Coding Circle?</h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          <div className="bg-white p-8 rounded-lg shadow-lg shadow-cyan-500/50">
-            <h3 className="text-xl font-bold mb-4 ">DSA(Data Structures and Algorithms)</h3>
-            <p>Here is a guide for you to direct you on the roadmap of success....</p>
+      <section class="w-full  pt-7 pb-7 md:pt-20 md:pb-24">
+        <div class="box-border flex flex-col items-center content-center px-8 mx-auto leading-6 text-white border-0 border-gray-300 border-solid md:flex-row max-w-7xl lg:px-16">
+          <div class="box-border relative w-full max-w-md px-4 mt-5 mb-4 -ml-5 text-center bg-no-repeat bg-contain border-solid md:ml-0 md:mt-0 md:max-w-none lg:mb-0 md:w-1/2 xl:pl-10">
+            <img
+              src="https://cdn.devdojo.com/images/december2020/settings.png"
+              class="p-2 pl-6 pr-5 xl:pl-16 xl:pr-20 "
+            />
           </div>
-          <div className="bg-white p-8 rounded-lg shadow-lg shadow-cyan-500/50">
-            <h3 className="text-xl font-bold mb-4">Ongoing Projects With PCSB</h3>
-            <p>Get a chance to work with the PCSB club of PICT!</p>
-          </div>
-          <div className="bg-white p-8 rounded-lg shadow-lg shadow-cyan-500/50">
-            <h3 className="text-xl font-bold mb-4">CP(Competitive Programming)</h3>
-            <p>The roadmap to the journey on how to begin with Competitive Programming</p>
+
+          <div class="box-border order-first w-full text-white border-solid md:w-1/2 md:pl-10 md:order-none">
+            <h2 class="m-0 text-xl font-semibold leading-tight border-0 border-gray-300 lg:text-3xl md:text-2xl">
+              DSA(Data Structures and Algorithms)
+            </h2>
+            <p class="pt-4 pb-8 m-0 leading-7 text-white-700 border-0 border-gray-300 sm:pr-12 xl:pr-32 lg:text-lg">
+              The roadmap to the journey on how to begin with DSA
+            </p>
+            <ul class="p-0 m-0 leading-6 border-0 border-gray-300">
+              <li class="box-border relative py-1 pl-0 text-left text-white-500 border-solid">
+                <span class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full">
+                  <span class="text-sm font-bold">✓</span>
+                </span>
+                DSA is foundational for efficient data organization and
+                problem-solving in computer science.
+              </li>
+              <li class="box-border relative py-1 pl-0 text-left text-white-500 border-solid">
+                <span class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full">
+                  <span class="text-sm font-bold">✓</span>
+                </span>
+                Optimization through proper data structures and algorithms is
+                key to program efficiency.
+              </li>
+              <li class="box-border relative py-1 pl-0 text-left text-white-500 border-solid">
+                <span class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full">
+                  <span class="text-sm font-bold">✓</span>
+                </span>
+                DSA is essential for problem-solving in software development and
+                computer science.
+              </li>
+            </ul>
           </div>
         </div>
-      </div>
-    </section>
-  );
-};
+        <div class="box-border flex flex-col items-center content-center px-8 mx-auto mt-2 leading-6 text-white border-0 border-gray-300 border-solid md:mt-20 xl:mt-0 md:flex-row max-w-7xl lg:px-16">
+          <div class="box-border w-full text-white border-solid md:w-1/2 md:pl-6 xl:pl-32">
+            <h2 class="m-0 text-xl font-semibold leading-tight border-0 border-gray-300 lg:text-3xl md:text-2xl">
+              CP(Competitive Programming)
+            </h2>
+            <p class="pt-4 pb-8 m-0 leading-7 text-white-700 border-0 border-gray-300 sm:pr-10 lg:text-lg">
+              The roadmap to the journey on how to begin with Competitive
+              Programming.
+            </p>
+            <ul class="p-0 m-0 leading-6 border-0 border-gray-300">
+              <li class="box-border relative py-1 pl-0 text-left text-white-500 border-solid">
+                <span class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full">
+                  <span class="text-sm font-bold">✓</span>
+                </span>
+                Various programming languages like Python, Java, and C++ serve
+                different purposes.
+              </li>
+              <li class="box-border relative py-1 pl-0 text-left text-white-500 border-solid">
+                <span class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full">
+                  <span class="text-sm font-bold">✓</span>
+                </span>
+                CP involves coding, testing, and debugging to create software
+                applications.
+              </li>
+              <li class="box-border relative py-1 pl-0 text-left text-white-500 border-solid">
+                <span class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full">
+                  <span class="text-sm font-bold">✓</span>
+                </span>
+                Continuous learning is vital in CP to adapt to evolving
+                technologies.
+              </li>
+            </ul>
+          </div>
 
-const CallToActionSection = () => {
-  return (
-    <section className="bg-gray-800 text-white py-16 text-center">
-      <div className="container mx-auto">
-        <h2 className="text-3xl font-bold mb-4">Ready to Level Up Your Coding Skills?</h2>
-        <p className="text-lg mb-8">Join Coding Circle today and embark on a journey of continuous learning!</p>
-        <a href="#" className="bg-white text-blue-600 font-bold py-2 px-6 rounded-full hover:bg-blue-200">
-          Join Now
-        </a>
-      </div>
-    </section>
-  );
-};
+          <div class="box-border relative w-full max-w-md px-4 mt-10 mb-4 text-center bg-no-repeat bg-contain border-solid md:mt-0 md:max-w-none lg:mb-0 md:w-1/2">
+            <img
+              src="https://cdn.devdojo.com/images/december2020/settings.png"
+              class="pl-4 sm:pr-10 xl:pl-10 lg:pr-32"
+            />
+          </div>
+        </div>
+      </section>
 
-// const Footer = () => {
-//   return (
-//     <footer className="bg-gray-900 text-white text-center py-8">
-//       <p>&copy; 2023 Coding Circle. All rights reserved.</p>
-//     </footer>
-//   );
-// };
+      <section class="py-20 bg-gray-600">
+        <div class="container items-center max-w-6xl px-4 px-10 mx-auto sm:px-20 md:px-32 lg:px-16">
+          <div class="flex flex-wrap items-center -mx-3">
+            <div class="order-1 w-full px-3 lg:w-1/2 lg:order-0">
+              <div class="w-full lg:max-w-md">
+                <h2 class="mb-4 text-3xl font-bold leading-tight tracking-tight sm:text-4xl font-heading">
+                  Jam-packed with all the tools you need to succeed!
+                </h2>
+                <p class="mb-4 font-medium tracking-tight text-white-400 xl:mb-6">
+                  Join Coding Circle today and embark on a journey of continuous
+                  learning!
+                </p>
+                <ul>
+                  <li class="flex items-center py-2 space-x-4 xl:py-3">
+                    <svg
+                      class="w-8 h-8 text-pink-500"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"
+                      ></path>
+                    </svg>
+                    <span class="font-medium text-white-500">
+                      Level Up Your Coding Skills
+                    </span>
+                  </li>
+                  <li class="flex items-center py-2 space-x-4 xl:py-3">
+                    <svg
+                      class="w-8 h-8 text-yellow-500"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"
+                      ></path>
+                    </svg>
+                    <span class="font-medium text-white-500">
+                      Networking with peers
+                    </span>
+                  </li>
+                  <li class="flex items-center py-2 space-x-4 xl:py-3">
+                    <svg
+                      class="w-8 h-8 text-green-500"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
+                      ></path>
+                    </svg>
+                    <span class="font-medium text-white-500">
+                      Getting Prepared for Placements!!
+                    </span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="w-full px-3 mb-12 lg:w-1/2 order-0 lg:order-1 lg:mb-0">
+              <img
+                class="mx-auto sm:max-w-sm lg:max-w-full"
+                src="https://cdn.devdojo.com/images/november2020/feature-graphic.png"
+                alt="feature image"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
 
+      <section class="flex items-center justify-center py-20  min-w-screen">
+        <div class="px-16 ">
+          <div class="container flex flex-col items-start mx-auto lg:items-center">
+            <p class="relative flex items-start justify-start w-full text-lg font-bold tracking-wider text-purple-500 uppercase lg:justify-center lg:items-center">
+              Don't just take our word for it
+            </p>
 
-const App = () => {
-  return (
-    
-    <div className="bg-gray-800 ">
-      
-      <HeroSection />
-      <FeaturesSection />
-      <CallToActionSection />
-      {/* <Footer /> */}
+            <h2 class="relative flex items-start justify-start w-full max-w-3xl text-5xl font-bold lg:justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                class="absolute right-0 hidden w-12 h-12 -mt-2 -mr-16 text-white-200 lg:inline-block"
+                viewBox="0 0 975.036 975.036"
+              >
+                <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+              </svg>
+              See what Programmers are saying
+            </h2>
+            <div class="block w-full h-0.5 max-w-lg mt-6 bg-purple-100 rounded-full"></div>
+
+            <div class="items-center justify-center w-full mt-12 mb-4 lg:flex">
+              <div class="flex flex-col items-start justify-start w-full h-auto mb-12 lg:w-1/3 lg:mb-0">
+                <div class="flex items-center justify-center">
+                  <div class="w-16 h-16 mr-4 overflow-hidden bg-gray-200 rounded-full">
+                    <img
+                      src="https://media.licdn.com/dms/image/D4D35AQHatC3171UG9Q/profile-framedphoto-shrink_400_400/0/1689487574366?e=1695578400&v=beta&t=kIkvW5XyhmJxIaB1D_2PHSmhWONIl2i9behLz2C7Pbg"
+                      class="object-cover w-full h-full"
+                    />
+                  </div>
+                  <div class="flex flex-col items-start justify-center">
+                    <h4 class="font-bold text-white-800">Tanisha</h4>
+                    <p class="text-white-600">Director at Coding Circle</p>
+                  </div>
+                </div>
+                <blockquote class="mt-8 text-lg text-white-500">
+                  "Coding like poetry should be short and concise."
+                </blockquote>
+              </div>
+              <div class="flex flex-col items-start justify-start w-full h-auto px-0 mx-0 mb-12 border-l border-r border-transparent lg:w-1/3 lg:mb-0 lg:px-8 lg:mx-8 lg:border-gray-200">
+                <div class="flex items-center justify-center">
+                  <div class="w-16 h-16 mr-4 overflow-hidden bg-gray-200 rounded-full">
+                    <img
+                      src="https://media.licdn.com/dms/image/D4D35AQHatC3171UG9Q/profile-framedphoto-shrink_400_400/0/1689487574366?e=1695578400&v=beta&t=kIkvW5XyhmJxIaB1D_2PHSmhWONIl2i9behLz2C7Pbg"
+                      class="object-cover w-full h-full"
+                    />
+                  </div>
+                  <div class="flex flex-col items-start justify-center">
+                    <h4 class="font-bold text-white-800">Tanisha</h4>
+                    <p class="text-white-600">Director at Coding Circle</p>
+                  </div>
+                </div>
+                <blockquote class="mt-8 text-lg text-white-500">
+                  "It’s not a bug; it’s an undocumented feature"
+                </blockquote>
+              </div>
+              <div class="flex flex-col items-start justify-start w-full h-auto lg:w-1/3">
+                <div class="flex items-center justify-center">
+                  <div class="w-16 h-16 mr-4 overflow-hidden bg-gray-200 rounded-full">
+                    <img
+                      src="https://media.licdn.com/dms/image/D4D35AQHatC3171UG9Q/profile-framedphoto-shrink_400_400/0/1689487574366?e=1695578400&v=beta&t=kIkvW5XyhmJxIaB1D_2PHSmhWONIl2i9behLz2C7Pbg"
+                      class="object-cover w-full h-full"
+                    />
+                  </div>
+                  <div class="flex flex-col items-start justify-center">
+                    <h4 class="font-bold text-white-800">Tanisha</h4>
+                    <p class="text-white-600">Director at Coding Circle</p>
+                  </div>
+                </div>
+                <blockquote class="mt-8 text-lg text-white-500">
+                  "First, solve the problem. Then, write the code"
+                </blockquote>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-   
   );
 };
 
-export default App;
-
+export default Landing_page;
